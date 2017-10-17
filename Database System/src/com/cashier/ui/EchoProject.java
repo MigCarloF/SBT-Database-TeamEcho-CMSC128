bc3a6dfb8fdc08ac479929c6db886ad88b356854
alboresallyssa <asalbores@up.edu.ph>
2017-10-17 18:39:32 +0800

    v2

--- Database System/src/com/cashier/ui/EchoProject.java
+++ Database System/src/com/cashier/ui/EchoProject.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package echo;
+package com.cashier.ui;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
--- Database System/src/com/cashier/ui/FXMLArrivalWindowController.java
+++ Database System/src/com/cashier/ui/FXMLArrivalWindowController.java
@@ -3,18 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package echo;
+package com.cashier.ui;
 
-import java.awt.Checkbox;
-import java.io.IOException;
-import java.net.URL;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.ResourceBundle;
+
+import com.database.Database;
+import com.database.DummyDatabase;
+import com.database.DummyDatabaseBus;
+import com.database.Fee;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -29,6 +24,14 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.ResourceBundle;
+
 /**
  *
  * @author alboresallyssa
@@ -37,28 +40,28 @@ public class FXMLArrivalWindowController implements Initializable {
     //These items are for the buttons in arrival window
     @FXML private Button busPrintButton;
     @FXML private Button minibusPrintButton;
-    
+
     @FXML private Button transactButton;
     @FXML private Button voidButton;
     @FXML private Button cashierButton;
     @FXML private Button logoutButton;
-    
+
     //These items are for the combo boxes in arrival window
     @FXML private ComboBox busFDD;
     @FXML private ComboBox minibusFDD;
-    
+
     //These items are for the check boxes in arrival window
     @FXML private CheckBox arrivalFee;
     @FXML private CheckBox loadingFee;
-    
+
     //These items are for the text fields in arrival window
     @FXML private TextField busNumber;
     @FXML private TextField plateNumber;
-    
+
     @FXML private ObservableList<Fee> fees;
     @FXML private DummyDatabase database;
     @FXML private DummyDatabaseBus databaseBus;
-    
+
     /**
      * When this method is called, a new window will appear.
      * The pop up window is the void window.
@@ -68,73 +71,80 @@ public class FXMLArrivalWindowController implements Initializable {
     public void voidButtonPushed(ActionEvent event) throws IOException {
         Parent voidWindowParent = FXMLLoader.load(getClass().getResource("FXMLVoidWindow.fxml"));
         Scene voidWindowScene = new Scene(voidWindowParent);
-        
+
         //This line gets the Stage information
         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-        
+
         window.setScene(voidWindowScene);
         window.show();
     }
-    
+
     /**
      * When this method is called, a pop up window will appear.
      * The pop up window is the confirmation window for the BUS.
      * @param event
      * @throws java.io.IOException
      */
-    public void busConfirmButtonPushed(ActionEvent event) throws IOException {
+    public void busConfirmButtonPushed(ActionEvent event) throws IOException, InterruptedException {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
         LocalDate localDate = LocalDate.now();
-        
+
         Date date = new java.util.Date();
         SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
         String dateFormat = sdf.format(date.getTime());
-        
-        // to get the checked box in the window 
+
+        // to get the checked box in the window
         // TODO: database, if both are allowed to be checked
         String typeOfFee = "";
+        boolean paidArrival = false;
+        boolean paidLoading = false;
         if (arrivalFee.isSelected()) {
-            typeOfFee = "Arrival Fee";
+            paidArrival = true;
         } if (loadingFee.isSelected()) {
-            typeOfFee = "Loading Fee";
+            paidLoading = true;
         }
-        
+
         String busNum = busNumber.getText();
-        
-        // to get the selected franchise company in the window 
+
+        // to get the selected franchise company in the window
         // TODO: database
         String franchiseSelected = busFDD.getValue().toString();
-        //System.out.println("Franchise: " + franchiseSelected + ", Type Of Fee: " + typeOfFee 
-        //+ ", and Bus number: " + busNum + ", Time: " + dateFormat + ", Date: " + localDate);
-        
-        Bus temp = new Bus(franchiseSelected, busNum);
-        Fee forDatabase =  new Fee(true, false, dateFormat, "", "Cashier 01", localDate, temp);
+        System.out.println("Franchise: " + franchiseSelected + ", Type Of Fee: " + typeOfFee
+                + ", and Bus number: " + busNum + ", Time: " + dateFormat + ", Date: " + localDate);
+
+        Fee forDatabase =  new Fee(paidArrival, paidLoading, dateFormat, "", "Cashier 01", localDate, busNum);
+        Database database = Database.database;
+        database.addFee(forDatabase);
+        Database.database.displayFees();
     }
-    
+
     /**
      * When this method is called, a pop up window will appear.
      * The pop up window is the confirmation window for the MINIBUS.
      * @param event
      * @throws java.io.IOException
      */
-    public void minibusConfirmButtonPushed(ActionEvent event) throws IOException {
+    public void minibusConfirmButtonPushed(ActionEvent event) throws IOException, InterruptedException {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
         LocalDate localDate = LocalDate.now();
-        
+
         Date date = new java.util.Date();
         SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
         String dateFormat = sdf.format(date.getTime());
-        
-        // to get the selected franchise company in the window 
+
+        // to get the selected franchise company in the window
         // TODO: database
         String franchiseSelected = minibusFDD.getValue().toString();
         String plateNum = plateNumber.getText();
         System.out.println(franchiseSelected);
-        
-        Bus temp = new Bus(plateNum, franchiseSelected);
-        Fee forDatabase =  new Fee(true, false, dateFormat, "", "Cashier 01", localDate, temp);
+
+        //Bus temp = new Bus(plateNum, franchiseSelected);
+        Fee forDatabase =  new Fee(true, false, dateFormat, "", "Cashier 01", localDate, plateNum);
+        Database database = Database.database;
+        database.addFee(forDatabase);
+        Database.database.displayFees();
     }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         /**
@@ -146,7 +156,7 @@ public class FXMLArrivalWindowController implements Initializable {
         busFDD.setVisibleRowCount(3);
         busFDD.setEditable(true);
         busFDD.setPromptText("BUS");
-        
+
         /**
          * These items are for configuring the Combo Box.
          * MINIBUS Combo Box
