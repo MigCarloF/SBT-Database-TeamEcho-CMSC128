2292cc8905aa0b78423393f94333ab301949a5fa
Allyssa Albores <asalbores@up.edu.ph>
2017-10-21 17:06:52 +0800

    added files

---
+++ Database System/src/com/cashier/ui/FXMLExitWindowController.java
@@ -0,0 +1,37 @@
+package com.cashier.ui;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class FXMLExitWindowController {
+
+    @FXML
+    private JFXButton exitWindowPrint;
+
+    @FXML
+    private JFXTextField exitWindowOrNo;
+
+    @FXML
+    private JFXButton exitWindowCashier;
+
+    @FXML
+    private JFXButton exitWindowLogout;
+
+    @FXML
+    void exitWindowCashierPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void exitWindowLogoutPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void exitWindowPrintPressed(ActionEvent event) {
+
+    }
+
+}
---
+++ Database System/src/com/cashier/ui/FXMLVoidWindowController.java
@@ -0,0 +1,82 @@
+package com.cashier.ui;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXCheckBox;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class FXMLVoidWindowController {
+
+    @FXML
+    private JFXButton voidWindowSendRequest;
+
+    @FXML
+    private JFXComboBox<?> voidWindowReasonVoid;
+
+    @FXML
+    private JFXCheckBox voidWindowArrivalFee1;
+
+    @FXML
+    private JFXCheckBox voidWindowLoadingFee1;
+
+    @FXML
+    private JFXTextField voidWindowOrNo;
+
+    @FXML
+    private JFXComboBox<?> voidWindowBusCompany1;
+
+    @FXML
+    private JFXComboBox<?> voidWindowBusCompany2;
+
+    @FXML
+    private JFXTextField voidWindowBusNumber1;
+
+    @FXML
+    private JFXTextField voidWindowBusNumber2;
+
+    @FXML
+    private JFXCheckBox voidWindowArrivalFee2;
+
+    @FXML
+    private JFXCheckBox voidWindowLoadingFee2;
+
+    @FXML
+    private JFXButton voidWindowTransact;
+
+    @FXML
+    private JFXButton voidWindowVoid;
+
+    @FXML
+    private JFXButton voidWindowCashier;
+
+    @FXML
+    private JFXButton voidWindowLogout;
+
+    @FXML
+    void voidWindowCashierPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void voidWindowLogoutPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void voidWindowSendRequestPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void voidWindowTransactPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void voidWindowVoidPressed(ActionEvent event) {
+
+    }
+
+}
